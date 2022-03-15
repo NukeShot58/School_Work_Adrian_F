f7ed6f7d983845b0303484991fbb431153331b5b
uczeń <adrian.filip@vp.pl>
2022-03-15 12:40:47 +0100

    .

---
+++ Obiect_Oriented_Programming/Zad.1/Samochod.js
@@ -0,0 +1,9 @@
+class Samochod{
+    constructor(marka,model,cena,rocznik,spalenie){
+        this.marka = marka;
+        this.model = model;
+        this.cena = cena;
+        this.rocznik = rocznik;
+        this.spalenie = spalenie;
+    }
+}
