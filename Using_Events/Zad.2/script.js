d09138d80b7959446c0b20c45fc1b757ecf42ed0
NukeShot58 <adrian.filip@vp.pl>
2022-04-12 12:27:54 +0200

    .

--- Using_Events/Zad.2/script.js
+++ Using_Events/Zad.2/script.js
@@ -3,3 +3,7 @@ div.addEventListener('mouseover', (e) => {
     e.preventDefault();
     div.textContent = "Works";
 })
+div.addEventListener('dblclick', (e) => {
+    e.preventDefault();
+    div.style.color = 'red';
+})
