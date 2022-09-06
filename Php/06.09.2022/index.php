aad98c7c0a922f0a493c9fdb53d3ef0e5dc2042a
NukeShot58 <adrian.filip@vp.pl>
2022-09-06 08:35:47 +0200

    Starting php entry and first code

---
+++ Php/06.09.2022/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php basics</title>
+</head>
+<body>
+    <h1>First php script</h1>
+    <!-- Run throught http server(Xampp) -->
+    <?php
+    echo('Hello World')
+    ?>
+</body>
+</html>
