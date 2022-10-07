631ff6fc9d2af8a0e134c39817ba1a6653248384
NukeShot58 <adrian.filip@vp.pl>
2022-10-07 07:17:42 +0200

    '.'

---
+++ Php/30.09.2022/Ex.1/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $data = new DateTime();
+    echo $data->format('Y-m-d H:i:s');
+    ?>
+</body>
+</html>
